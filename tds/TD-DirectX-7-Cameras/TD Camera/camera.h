55ccdf461832f5ee906f098ae45de3f04d193172
ezjonas <ezjonas@gmail.com>
2011-02-04 13:13:07 +0100

    laser dot textures

--- tds/TD-DirectX-7-Cameras/TD Camera/camera.h
+++ tds/TD-DirectX-7-Cameras/TD Camera/camera.h
@@ -1,14 +1,6 @@
-//////////////////////////////////////////////////////////////////////////////////////////////////
-// 
 // File: camera.h
-// 
 // Author: Frank Luna (C) All Rights Reserved
-//
-// System: AMD Athlon 1800+ XP, 512 DDR, Geforce 3, Windows XP, MSVC++ 7.0 
-//
 // Desc: Defines a camera's position and orientation.
-//         
-//////////////////////////////////////////////////////////////////////////////////////////////////
 
 #ifndef __cameraH__
 #define __cameraH__
@@ -27,13 +19,13 @@ public:
 	void strafe(float units); // left/right
 	void fly(float units);    // up/down
 	void walk(float units);   // forward/backward
-	
+
 	void pitch(float angle); // rotate on right vector
 	void yaw(float angle);   // rotate on up vector
 	void roll(float angle);  // rotate on look vector
 
-	void getViewMatrix(D3DXMATRIX * V); 
-	void setCameraType(CameraType cameraType); 
+	void getViewMatrix(D3DXMATRIX * V);
+	void setCameraType(CameraType cameraType);
 	void getPosition(D3DXVECTOR3 * pos);
 	void setPosition(D3DXVECTOR3 * pos);
 
