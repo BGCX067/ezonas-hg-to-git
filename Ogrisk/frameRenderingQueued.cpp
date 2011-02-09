ad7c785bb41e590c8818f0440024309b3c9d518d
ezjonas <ezjonas@gmail.com>
2011-02-09 14:14:01 +0100

    confs etc

--- Ogrisk/frameRenderingQueued.cpp
+++ Ogrisk/frameRenderingQueued.cpp
@@ -9,11 +9,13 @@ bool Application :: frameRenderingQueued(const FrameEvent & evt)
 	Ogre :: Vector3 translate(0, 0, 0);
 	if (keyboard -> isKeyDown(KC_W)) translate += Ogre :: Vector3(0, 0, -1);
 	if (keyboard -> isKeyDown(KC_S)) translate += Ogre :: Vector3(0, 0, 1);
+
 	if (keyboard -> isKeyDown(KC_A)) translate += Ogre :: Vector3(-1, 0, 0);
 	if (keyboard -> isKeyDown(KC_D)) translate += Ogre :: Vector3(1, 0, 0);
 
 	if (keyboard -> isKeyDown(KC_Q)) translate += Ogre :: Vector3(0, -1, 0);
 	if (keyboard -> isKeyDown(KC_E)) translate += Ogre :: Vector3(0, 1, 0);
+
 	camera -> moveRelative(translate * evt.timeSinceLastFrame * moving_speed);
 
 #ifdef FPS_CAM
