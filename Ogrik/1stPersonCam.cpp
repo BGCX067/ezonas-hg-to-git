52dc41e65a4c36cd56f4c53734f58332a3f5614d
ezjonas <ezjonas@gmail.com>
2011-02-22 17:11:37 +0100

    words

--- Ogrik/1stPersonCam.cpp
+++ Ogrik/1stPersonCam.cpp
@@ -1,12 +1,21 @@
 #include "stdafx.h"
 
-FPersonCam :: FPersonCam(Camera * camera, SceneManager * scmgr):
+FPersonCam :: FPersonCam(Camera * camera, SceneManager * scmagr):
 
-	cam_roll	(new SceneNode),
-	cam_yaw		(new SceneNode),
-	cam_pitch	(new SceneNode),
-	cam_node	(new SceneNode),
-	scmgr		(scmgr),
+	cam_roll	(scmgr->createSceneNode("cam_roll")),
+	cam_yaw		(scmgr->createSceneNode("cam_yaw")),
+	cam_pitch	(scmgr->createSceneNode("cam_pitch")),
+	cam_node	(scmgr->createSceneNode("cam_node")),
+	scmgr		(scmagr),
 	cam			(camera)
 
-{}
+{
+	scmgr -> getRootSceneNode() ->
+					addChild(cam_node);
+	cam_node ->		addChild(cam_yaw);
+	cam_yaw ->		addChild(cam_pitch);
+	cam_pitch ->	addChild(cam_roll);
+	cam_roll -> 	addChild(cam_yaw);
+	
+	cam_roll -> 	attachObject(cam);
+}
