91cf0b2088fb41bc8635d8dc466bd73cf810d969
ezjonas <devnull@localhost>
2011-03-12 17:23:44 +0100

    fd

--- Ogrish/FrameRenderingQueued.cpp
+++ Ogrish/FrameRenderingQueued.cpp
@@ -3,9 +3,6 @@
 // ############################# frameStarted #############################
 bool Application :: frameRenderingQueued(const FrameEvent & evt)
 {
-// keyboard and mouse capturing
-	// raycast -> execute();
-//	cursor_ray = camera -> getCameraToViewportRay
 	return fpersoncam -> update(evt.timeSinceLastFrame);
 }
 
