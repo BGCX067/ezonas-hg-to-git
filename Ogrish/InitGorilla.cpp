3b51aa6dce640b23f7ea9206995d96126d5bf530
BLAAA <devnull@localhost>
2011-03-23 17:22:16 +0100

    rectangles rectangles !

--- Ogrish/InitGorilla.cpp
+++ Ogrish/InitGorilla.cpp
@@ -2,18 +2,26 @@
 
 void Application :: InitGorilla()
 {
-	string fontname("dejavuserif");
+	string fontname("test");
+
+	int atlas_index;
+	if(fontname == "dejavu") atlas_index = 9;
+	if(fontname == "dejavuserif") atlas_index = 12;
+	if(fontname == "test") atlas_index = 7;
+
 	mGorilla -> loadAtlas(fontname);
 	gor_screen = mGorilla -> createScreen(viewport, fontname);
 	gor_layer = gor_screen -> createLayer(0);
 
-	gor_caption[0] = gor_layer -> createCaption(12, 2, 2,
+	gor_rect = gor_layer -> createRectangle(0,0,300, 50);
+	gor_rect -> background_colour(ColourValue(0.3, 0.3, 0.3, 0.3));
+
+	gor_caption[0] = gor_layer -> createCaption(atlas_index, 2, 2,
 		//string("Does the lazy fox quickly jumps over the brown dog ?"));
 		string("Counter Cake is Zevil"));
-	gor_caption[1] = gor_layer -> createCaption(12, 200, 2,
+	gor_caption[1] = gor_layer -> createCaption(atlas_index, 200, 2,
 		string("-"));
-	gor_rect = gor_layer -> createRectangle(100,100);
-	gor_rect -> background_colour(ColourValue(1, 0.3, 0.3));
+
 	//gor_rect -> border(10, ColourValue());
 }
 
